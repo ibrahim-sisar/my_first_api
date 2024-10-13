fca9f74f1d75ca8236520f9494cedb48178437a8
ibrahim abu al roos <ibrahim.sisar@gmail.com>
2024-10-13 07:43:34 +0300

    Delete emarket directory

--- emarket/product/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import CRUDS
-
-class productSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=CRUDS
-        fields='__all__'
--- emarket/product/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.show_all),
-    path('single/<pk>/',views.single_obj),
-    path('single2/<text>/',views.search),
-   
-]
